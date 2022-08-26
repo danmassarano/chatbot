e4ea3ef30900d2938e6f820f29d75920dc5464bc
Dan <danielmassarano@gmail.com>
2022-08-26 19:59:04 +0100

    Refactored get_tweets to return text #MINOR

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,22 +1,22 @@
-from data_cleanse import clean_input_files
-from extract import get_all_tweets
-from extract import get_api_client
-from extract import get_output_file
-from markov_chain import load_input_files
-from markov_chain import output_sentence
-from markov_chain import output_short_sentence
-from markov_chain import train_text_model
+from src.data_cleanse import clean_input_files
+from src.extract import get_all_tweets
+from src.extract import get_api_client
+from src.extract import get_output_file
+from src.extract import write_to_output_file
+from src.markov_chain import load_input_files
+from src.markov_chain import output_sentence
+from src.markov_chain import output_short_sentence
+from src.markov_chain import train_text_model
 
 output_file, writer = get_output_file("twitter_dump")
 api = get_api_client()
-get_all_tweets("edthewlis", api, writer)
-output_file.close()
+tweets = get_all_tweets("edthewlis", api)
+write_to_output_file(writer, tweets, output_file)
 # get_all_tweets("elonmusk", "twitter_dump")
+
 clean_input_files()
 
-
 training_data = load_input_files()
-
 text_model = train_text_model(training_data)
 
 print("Print five sentences:\n")
