f564274305e0d28c12d5fb78e697c759a6a389ff
Dan <danielmassarano@gmail.com>
2022-08-26 18:20:20 +0100

    Refactored extractor #MINOR

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,13 +1,16 @@
 from data_cleanse import clean_input_files
+from extract import get_all_tweets
+from extract import get_api_client
+from extract import get_output_file
+from markov_chain import load_input_files
+from markov_chain import output_sentence
+from markov_chain import output_short_sentence
+from markov_chain import train_text_model
 
-from src.extract import get_all_tweets
-from src.markov_chain import load_input_files
-from src.markov_chain import output_sentence
-from src.markov_chain import output_short_sentence
-from src.markov_chain import train_text_model
-
-
-get_all_tweets("edthewlis", "twitter_dump")
+output_file, writer = get_output_file("twitter_dump")
+api = get_api_client()
+get_all_tweets("edthewlis", api, writer)
+output_file.close()
 # get_all_tweets("elonmusk", "twitter_dump")
 clean_input_files()
 
