5f79664506465d1e079d799445b39fbfed9d65ed
Dan <danielmassarano@gmail.com>
2022-09-19 16:32:34 +0100

    Improved data testing process

--- tests/test_markov_chain.py
+++ tests/test_markov_chain.py
@@ -2,13 +2,17 @@
 import os
 import sys
 import unittest
+from os.path import exists
 
+from test_constants import CLEANED_DATA_DIR
 from test_constants import EXPECTED_DATA_DIR
+from test_constants import UNSTAGED_FILE
 
 PROJECT_PATH = os.getcwd()
 SOURCE_PATH = os.path.join(PROJECT_PATH, "chatbot")
 sys.path.append(SOURCE_PATH)
 
+from chatbot.data_cleanse import unstage_cleansed_files  # noqa: E402
 from chatbot.markov_chain import load_input_files  # noqa: E402
 from chatbot.markov_chain import load_text_file  # noqa: E402
 from chatbot.markov_chain import output_sentence  # noqa: E402
@@ -56,18 +60,40 @@ class TestMarkovChain(unittest.TestCase):
 
     def test_output_sentence(self):
         """Test that a regular sentence is generated correctly."""
-        filename = "test_txt.txt"
-        training_data = load_input_files(EXPECTED_DATA_DIR, filename)
-        text_model = train_text_model(training_data)
-        for _ in range(5):
-            result = output_sentence(text_model)
-            self.assertIsNotNone(result)
+        expected_txt = f"{CLEANED_DATA_DIR}/{UNSTAGED_FILE}"
+        try:
+            if not os.path.exists(CLEANED_DATA_DIR):
+                os.makedirs(CLEANED_DATA_DIR)
+            unstage_cleansed_files(
+                EXPECTED_DATA_DIR, CLEANED_DATA_DIR, UNSTAGED_FILE
+            )
+            training_data = load_input_files(CLEANED_DATA_DIR, UNSTAGED_FILE)
+            text_model = train_text_model(training_data)
+            for _ in range(5):
+                result = output_sentence(text_model)
+                self.assertIsNotNone(result)
+        finally:
+            if exists(expected_txt):
+                os.remove(expected_txt)
+            if os.path.exists(CLEANED_DATA_DIR):
+                os.rmdir(CLEANED_DATA_DIR)
 
     def test_output_short_sentence(self):
         """Test that a regular sentence is generated correctly."""
-        filename = "test_txt.txt"
-        training_data = load_input_files(EXPECTED_DATA_DIR, filename)
-        text_model = train_text_model(training_data)
-        for _ in range(5):
-            result = output_short_sentence(text_model, 280)
-            self.assertIsNotNone(result)
+        expected_txt = f"{CLEANED_DATA_DIR}/{UNSTAGED_FILE}"
+        try:
+            if not os.path.exists(CLEANED_DATA_DIR):
+                os.makedirs(CLEANED_DATA_DIR)
+            unstage_cleansed_files(
+                EXPECTED_DATA_DIR, CLEANED_DATA_DIR, UNSTAGED_FILE
+            )
+            training_data = load_input_files(CLEANED_DATA_DIR, UNSTAGED_FILE)
+            text_model = train_text_model(training_data)
+            for _ in range(5):
+                result = output_short_sentence(text_model, 280)
+                self.assertIsNotNone(result)
+        finally:
+            if exists(expected_txt):
+                os.remove(expected_txt)
+            if os.path.exists(CLEANED_DATA_DIR):
+                os.rmdir(CLEANED_DATA_DIR)
